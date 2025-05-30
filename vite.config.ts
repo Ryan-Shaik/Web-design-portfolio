41a1e89678ba73adcb2d26341e35b60aa42879b4
Ryan Shaik <ryanshaik16@gmail.com>
2025-05-30 12:13:10 +0600

    config-updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: process.env.BASE_URL || '/',
+  base: process.env.BASE_URL || '/Web-design-portfolio',
 })
